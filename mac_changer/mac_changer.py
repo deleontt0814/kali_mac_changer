a3870bfa41d7d1233c05085dfcbea649fc0c1423
deleontt0814 <root@localhost.localdomain>
2019-07-17 10:10:36 -0700

    MAC changer

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+#successfully change MAC address using Kali's CLI
+
+import subprocess #process commands
+import optparse #for parsing
+import re #functiond for regex
+
+def get_arguments(): #obtains user input
+    parser = optparse.OptionParser()
+
+    #first input and allowable inputs
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    #second input and allowable inputs
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface: #if options.interface is empty
+        #code to handle
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac: #if options.new_mac is empty
+        #code to handle error
+        parser.error("[-] Please specify an interface, use --help for more info")
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    # subprocess.call("ifconfig " + interface + " down", shell=True)
+    # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+    # subprocess.call("ifconfig " + interface + " up", shell=True)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+
+    # isolates MAC address from ifconfig
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("Could no read MAC address")
+
+
+options = get_arguments() #change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface) #prints changed MAC address
+print("Current MAC = "+ str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
